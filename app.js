31115f4ce59a6034f031bbbad79ebe14370887aa
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2019-11-13 14:53:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let choice = [
+  {
+    name: 'rock',
+    beats: 'scissors',
+    compchoice: 1
+  },
+
+  {
+    name: 'paper',
+    beats: 'rock',
+    compchoice: 2
+  },
+
+  {
+    name: 'scissors',
+    beats: 'paper',
+    compchoice: 3
+  }
+]
+
+function play() {
+  document.querySelector('rock').className = "check2";
+};
+
+
+
+
+
