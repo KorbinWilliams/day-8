bc5436636614d27b8bd48b0e55186b16af879c11
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2019-11-13 16:14:48 -0700

    wasn't actually on to something back to square 2

--- app.js
+++ app.js
@@ -2,28 +2,35 @@ let choice = [
   {
     name: 'rock',
     beats: 'scissors',
-    compchoice: 1
+    compchoice: 1,
+    yourchoice: 'Your Choice'
   },
 
   {
     name: 'paper',
     beats: 'rock',
-    compchoice: 2
+    compchoice: 2,
+    yourchoice: 'Your Choice'
   },
 
   {
     name: 'scissors',
     beats: 'paper',
-    compchoice: 3
+    compchoice: 3,
+    yourchoice: 'Your Choice'
   }
 ]
 
-function play() {
-  if (document.querySelector('#rock').classList.contains("check1"))
-    return (document.querySelector('#rock').className = "check2")
-};
-
-
-
+// function play() {
+// if (document.querySelector('#rock').classList.contains("check1"))
+// return (document.querySelector('#rock').className = "check2")
+// };
+debugger;
+function play(name) {
+  let playerchoice = choice.find(c => c.title.toLowerCase() == name.toLowerCase())
+  document.querySelector("#rock").innerHTML = `
+  <h1 id="rock" class="check2"> ${playerchoice.yourchoice}</h1>
+  `;
+}
 
 
