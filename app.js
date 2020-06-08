bb95c3b6391a1793cb9fffa82146628fa58572ea
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-06-08 16:42:17 -0600

    Idk why im working on this

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-let choice = {
+let choices = [
   {
-  title: 'rock',
-    beats: 'scissors',
-      compchoice: 1,
-        yourchoice: 'Your Choice'
-},
+    title: "rock",
+    beats: "scissors",
+    compchoice: 1,
+    yourchoice: "Your Choice",
+  },
 
-{
-  title: 'paper',
-    beats: 'rock',
-      compchoice: 2,
-        yourchoice: 'Your Choice'
-},
+  {
+    title: "paper",
+    beats: "rock",
+    compchoice: 2,
+    yourchoice: "Your Choice",
+  },
 
-{
-  title: 'scissors',
-    beats: 'paper',
-      compchoice: 3,
-        yourchoice: 'Your Choice'
-}
-}
+  {
+    title: "scissors",
+    beats: "paper",
+    compchoice: 3,
+    yourchoice: "Your Choice",
+  },
+];
 
+let rockChoice = document.querySelector("#rock");
+let paperChoice = document.querySelector("#paper");
+let scissorsChoice = document.querySelector("#scissors");
 
-debugger;
 function play(name) {
-  let playerchoice = choice.find(c => c.title.toLowerCase() == name.toLowerCase())
-  if (name == 'rock') {
-    document.querySelector("#rock").innerHTML = `
-  <h1 class="check2"><span id="rock">${playerchoice.yourchoice}</span></h1>
-  `}
-  else if (name == 'paper') {
-    document.querySelector("#paper").innerHTML = `
-  <h1 class="check2"><span id="paper">${playerchoice.yourchoice}<span/></h1>
-  `}
-  else {
-    document.querySelector("#scissors").innerHTML = `
-  <h1 class="check2"><span id="scissors">${playerchoice.yourchoice}</span></h1>
-  `}
+  console.log(choices);
+  let compChoice = Math.random() * 3 + 1;
+  console.log(compChoice);
+  // return Math.random() * (max - min) + min;
+  if (name.toLowerCase() == choices[0].title) {
+    rockChoice.textContent = "Your Choice";
+  } else if (name.toLowerCase() == choices[1].title) {
+    paperChoice.textContent = "Your Choice";
+  } else {
+    scissorsChoice.textContent = "Your Choice";
+  }
 }
 
-play(name);
-
-
-
+console.log();
