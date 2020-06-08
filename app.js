367b4aaef3faa123d4ce6ddb5b9021ab7b79a15d
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-06-08 17:45:26 -0600

    fixed it

--- app.js
+++ app.js
@@ -21,15 +21,34 @@ let choices = [
   },
 ];
 
-let rockChoice = document.querySelector("#rock");
-let paperChoice = document.querySelector("#paper");
-let scissorsChoice = document.querySelector("#scissors");
+let rockChoice = document.querySelector("#pRock");
+let paperChoice = document.querySelector("#pPaper");
+let scissorsChoice = document.querySelector("#pScissors");
+
+function getCompChoice() {
+  let min = Math.ceil(0);
+  let max = Math.floor(3);
+  let computerChoice =
+    choices[Math.floor(Math.random() * (max - min)) + min].title;
+  let compChoice = `#c${computerChoice}`;
+  console.log(compChoice);
+  return compChoice;
+}
+
+function clearBoard() {
+  document.querySelector("#crock").textContent = "";
+  document.querySelector("#pRock").textContent = "";
+  document.querySelector("#cpaper").textContent = "";
+  document.querySelector("#pPaper").textContent = "";
+  document.querySelector("#cscissors").textContent = "";
+  document.querySelector("#pScissors").textContent = "";
+}
 
 function play(name) {
-  console.log(choices);
-  let compChoice = Math.random() * 3 + 1;
-  console.log(compChoice);
-  // return Math.random() * (max - min) + min;
+  clearBoard();
+  let compChoice = document.querySelector(getCompChoice());
+  console.log(compChoice + " this");
+  compChoice.textContent = "Comp Choice";
   if (name.toLowerCase() == choices[0].title) {
     rockChoice.textContent = "Your Choice";
   } else if (name.toLowerCase() == choices[1].title) {
